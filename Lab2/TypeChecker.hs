17941e572af63dfa3a8f05a8f9909c49a2ce604f
William Dahlberg <dahlberg.william@gmail.com>
2015-11-19 15:33:51 +0100

    start lab2

--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -4,6 +4,11 @@ import AbsCPP
 import PrintCPP
 import ErrM
 
+type Env = (Sig, [Context])
+type Sig = Map Id ([Type], Type)
+type Context = Map Id Type
 
 typecheck :: Program -> Err ()
 typecheck p = return ()
+
+inferExp :: Env -> 
