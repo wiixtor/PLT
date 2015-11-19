05dd5b589470dbff0b5dd87def23fe3bc55c5b59
William Dahlberg <dahlberg.william@gmail.com>
2015-11-19 16:01:24 +0100

    fixat absCPP

--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -3,6 +3,7 @@ module TypeChecker where
 import AbsCPP
 import PrintCPP
 import ErrM
+import Data.Map
 
 type Env = (Sig, [Context])
 type Sig = Map Id ([Type], Type)
@@ -12,36 +13,45 @@ typecheck :: Program -> Err ()
 typecheck p = return ()
 
 checkStm :: Env -> Type -> Stm -> Err Env
-	checkStm env val x = case x of
-		SExp exp -> do
-			inferExp env exp
-			return env
-		SDecl type’ x ->
-			updateVar env id type’
-		SWhile exp stm -> do
-			checkExp env Type_bool exp
-			checkStm env val stm
+checkStm env val x = case x of
+	SExp exp -> do
+		inferExp env exp
+		return env
+	SDecls type' x ->
+		updateVar env id type'
+	SWhile exp stm -> do
+		checkExp env Type_bool exp
+		checkStm env val stm
 
 checkExp :: Env -> Type -> Exp -> Err ()
-	checkExp env typ exp = do
-		typ2 <- inferExp env exp
-		if (typ2 == typ) then
-			return ()
-		else
-			fail $ "type of " ++ printTree exp
+checkExp env typ exp = do
+	typ2 <- inferExp env exp
+	if (typ2 == typ) then
+		return ()
+	else
+		fail $ "type of " ++ printTree exp
 
 inferExp :: Env -> Exp -> Err Type
-	inferExp env x = case x of
-		ETrue -> return Type_bool
-		EInt n -> return Type_int
-		EId id -> lookVar env id
-		EAdd exp0 exp -> inferArithmBin env exp0 exp
+inferExp env x = case x of
+	ETrue -> return Type_bool
+	EInt n -> return Type_int
+	EId id -> lookVar env id
+	EPlus exp0 exp -> inferArithmBin env exp0 exp
 
 lookVar :: Env -> Id -> Err Type
+lookVar = undefined
+
 lookFun :: Env -> Id -> Err ([Type],Type)
+lookFun = undefined
+
 updateVar :: Env -> Id -> Type -> Err Env
+updateVar = undefined
+
 updateFun :: Env -> Id -> ([Type],Type) -> Err Env
+updateFun = undefined
+
 newBlock :: Env -> Err Env
+newBlock = undefined
 
 emptyEnv :: Env
-emptyEnv = 
+emptyEnv = ((), [])
