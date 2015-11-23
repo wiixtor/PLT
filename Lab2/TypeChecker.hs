f4e8daa89afea3f6f2f608400dc4b441e26a9b25
William Dahlberg <dahlberg.william@gmail.com>
2015-11-23 14:58:02 +0100

    checdef

--- Lab2/TypeChecker.hs
+++ Lab2/TypeChecker.hs
@@ -10,19 +10,29 @@ type Sig = Map Id ([Type], Type)
 type Context = Map Id Type
 
 typecheck :: Program -> Err ()
-typecheck p = return ()
+typecheck (PDefs []) = do
+	return ()
+typecheck (PDefs (d:ds)) = do
+	ok <- checkDef emptyEnv d
+	if (ok == Ok ()) then
+		typecheck (PDefs ds)
+	else 
+		fail $ "Def "-- ++ printTree d
 
-checkStm :: Env -> Type -> Stm -> Err Env
+checkDef :: Env -> Def -> Err ()
+checkDef env def = undefined
+
+{-checkStm :: Env -> Type -> Stm -> Err Env
 checkStm env val x = case x of
 	SExp exp -> do
 		inferExp env exp
 		return env
-	SDecls type' x ->
-		updateVar env id type'
+	SDecls dType x ->
+		updateVar env id dType
 	SWhile exp stm -> do
 		checkExp env Type_bool exp
 		checkStm env val stm
-
+-}
 {- 
  | SInit Type Id Exp
  | SReturn Exp
@@ -36,7 +46,7 @@ checkExp env typ exp = do
 	if (typ2 == typ) then
 		return ()
 	else
-		fail $ "type of " ++ printTree exp
+		fail $ "type of " -- ++ printTree exp
 
 inferExp :: Env -> Exp -> Err Type
 inferExp env x = case x of
@@ -72,19 +82,19 @@ inferArithmBin :: Env -> Exp -> Exp -> Err Type
 inferArithmBin env a b = do
 	typ <- inferExp env a
 	if elem typ [Type_int, Type_double] then do
-		checkExp env b typ
+		checkExp env typ b
 		return typ
 	else
-		fail $ "type of expression " ++ printTree exp -- 
+		fail $ "type of expression "-- ++ printTree exp -- 
 
 inferComparison :: Env -> Exp -> Exp -> Err Type
-inferArithmBin env a b = do
+inferComparison env a b = do
 	typ <- inferExp env a
 	if elem typ [Type_int, Type_double, Type_bool] then do
-		checkExp env b typ
+		checkExp env typ b
 		return typ
 	else
-		fail $ "type of expression " ++ printTree exp -- 
+		fail $ "type of expression " -- ++ printTree exp -- 
 
 lookVar :: Env -> Id -> Err Type
 lookVar = undefined
@@ -102,4 +112,4 @@ newBlock :: Env -> Err Env
 newBlock = undefined
 
 emptyEnv :: Env
-emptyEnv = ((), [])
+emptyEnv = undefined
