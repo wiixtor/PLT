0e571a24ffe4f1174f2847597fbd9c2742aca4ce
Wiixtor <wiixtor@gmail.com>
2015-11-27 13:42:49 +0100

    skellington interpreter

--- Lab2/Interpreter.hs
+++ Lab2/Interpreter.hs
@@ -7,5 +7,23 @@ import ErrM
 import Control.Monad
 import qualified Data.Map as M
 
+VInteger. Value ::= Integer ;
+VDouble. Value ::= Double ;
+VVoid. Value ::= ;
+VUndefined. Value ::= ;
+
+{-
+<Value,Env> eval (Env env, Exp e)
+Env exec (Env env, Statement s)
+Void exec (Program p) //interpret
+Value look (Ident x, Env env)
+Fun look (Ident x, Env env)
+Env extend (Env env, Ident x, Value v)
+Env extend (Env env, Definition d)
+Env push (Env env)
+Env pop (Env env)
+Env empty ()
+-}
+
 interpret :: Program -> IO ()
 interpret p = putStrLn "no interpreter yet"
