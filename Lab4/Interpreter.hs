990c3498e8b70ad073786e6ec11c8185ff489e6f
William Dahlberg <dahlberg.william@gmail.com>
2016-01-07 14:48:19 +0100

    changed from state monad to type env

--- Lab4/Interpreter.hs
+++ Lab4/Interpreter.hs
@@ -6,14 +6,14 @@ import qualified Data.Map as Map
 import Control.Monad
 import Control.Monad.State
 import AbsFun
+import System.Environment
 
 data EvStrat = CallByValue | CallByName
 
-data Env = Env {
-    envStrat :: EvStrat,
-    envFuns :: Map String Exp,
-    envValues :: [Map String Value]
-}
+type Env = (EvStrat, Functions, Values)
+type Functions = Map String Exp
+type Values = [Map String Value]
+
 
 data Value = VInt Integer | VAbs String Exp Substitution
 
@@ -21,29 +21,22 @@ data Closure = Clos Exp Substitution
 
 type Substitution = Map String Closure
 
---state transformer monad
-type M a = StateT Env IO a
-
-interpret :: EvStrat -> Program -> M ()
+interpret :: EvStrat -> Program -> IO ()
 interpret evstrat (Prog defs) = do
-    evalStateT f emptyEnv
+    newenv <- emptyEnv
+    e <- foldM
+        updateFun
+        newenv
+        defs
+    exp <- lookFun e (Ident "main")
     return ()
-  where
-    f :: M ()
-    f = do
-        mapM
-            updateFun
-            defs
-        exp <- lookFun (Ident "main")
-       -- eva lexp
-        return ()
+ 
 
-
-{- eval :: Env -> Closure -> Closure
-eval genv clos = ev clos
+eval :: Env -> Closure -> IO Closure
+eval (strat, funs, vals) (Clos e s) = ev (Clos e s)
   where
-    ev :: Closure -> Closure
-    ev = case clos of
+    ev :: Closure -> IO Closure
+    ev = case e of
         EVar id -> undefined
         EInt int -> undefined -- Clos (Eint int) sueb
         EAdd exp1 exp2 -> undefined
@@ -51,44 +44,57 @@ eval genv clos = ev clos
         ELt exp1 exp2 -> undefined
         EIf exp1 exp2 exp3 -> undefined
         EAbs id exp -> undefined
-        EApp f a ->
-            let Clos (Eabs (Ident v) fbody) sub' = ev (Clos f sub)
-            in case envStrat genv of
-                CallByValue ->
-                    let a' = ev (Clos a sub)
-                    in ev (Clos fbody (Map.insert v a' sub'))
+        EApp f a -> undefined {-do
+            Clos (EAbs (Ident v) fbody) sub' <- ev (Clos f s)
+            case strat of
+                CallByValue -> do
+                    a' <- ev (Clos a s)
+                    ev (Clos fbody (Map.insert v a' sub'))
                 CallByName ->
-                    ev (Clos fbody (Map.insert v (Clos a sub) sub'))
+                    ev (Clos fbody (Map.insert v (Clos a s) sub'))
 -}
-push :: M ()
-push = do
-    env <- get
-    put $ Env { envValues = (Map.empty : (envValues env)) }
+push :: Env -> IO Env
+push (a, b, v) = do
+    return $ (a, b, Map.empty : v)
 
-pop :: M ()
-pop = do
-    env <- get
-    put $ Env {envValues = (tail $ envValues env)}
+pop :: Env -> IO Env
+pop (a, b, (v:vs)) = do
+    return $ (a, b, vs)
 
-emptyEnv :: Env
-emptyEnv = Env {envStrat = CallByValue, envFuns = Map.empty}
+emptyEnv :: IO Env
+emptyEnv = return (CallByValue, Map.empty, (Map.empty:[]))
 
-lookFun :: Ident -> M Exp
-lookFun (Ident id) = do
-    env <- get
-    return $ envFuns env Map.! id 
+lookFun :: Env -> Ident -> IO Exp
+lookFun (_, f, _) (Ident id) = do
+    return $ f Map.! id 
 
-updateFun :: Def -> M ()
-updateFun (DDef (Ident funid) args exp) = do
-    env <- get
-    put $ env { envFuns = Map.insert funid exp (envFuns env) }
+updateFun :: Env -> Def -> IO Env
+updateFun (a, f, b) (DDef (Ident funid) args exp) = do
+    return (a, Map.insert funid exp f, b)
 
-lookVal :: Ident -> M Value
-lookVal (Ident id) = do
-    env <- get
-    return $ (head $ envValues env) Map.! id 
+lookVal :: Env -> Ident -> IO Value
+lookVal (a, b, vs) (Ident id) 
+    | null vs   = fail "value unfound" 
+    | otherwise =
+        case Map.lookup id (head vs) of
+            Nothing -> lookVal (a, b, (tail vs)) (Ident id)
+            Just x  -> return x
 
-updateVal :: Ident -> Value -> M ()
-updateVal (Ident id) val = do
-    env <- get
-    put $ env { envValues = (Map.insert id val (head $ (envValues env))) : (tail (envValues env) )}
+updateVal :: Env -> Ident -> Value -> IO Env
+updateVal (a, b, (v:vs)) (Ident id) val = do
+    return (a, b, Map.insert id val v : vs)
+
+
+
+
+
+
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let strat = case head args of
+                    "-v" -> CallByValue
+                    "-n" -> CallByName
+    interpret strat (last args)
