6be8b752c672af352be655c35d5002da90413e49
William Dahlberg <dahlberg.william@gmail.com>
2016-01-08 15:14:31 +0100

    buggg

--- Lab4/Interpreter.hs
+++ Lab4/Interpreter.hs
@@ -49,17 +49,17 @@ eval (strat, funs) (Clos e env) = ev (Clos e env)
             (VInt i1) <- ev (Clos exp1 env)
             (VInt i2) <- ev (Clos exp2 env)
             case (i1<i2) of 
-                True -> return 1
-                False -> return 0
+                True -> return (VInt 1)
+                False -> return (VInt 0)
         EIf exp1 exp2 exp3 -> do
             (VInt i1) <- ev (Clos exp1 env)
             case i1 of
                 1 -> do
                     c <- ev (Clos exp2 env)
-                    return ev (VClos c)
+                    return c
                 0 -> do
                     c <- ev (Clos exp3 env)
-                    return ev (VClos c)
+                    return c
         EAbs id exp -> undefined
         EApp f a -> undefined 
 
