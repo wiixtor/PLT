362c7d5c8bdbce798f016389a2d53ac133eed510
William Dahlberg <dahlberg.william@gmail.com>
2016-01-19 16:00:48 +0100

    bös

--- Lab3/CodeGenerator.hs
+++ Lab3/CodeGenerator.hs
@@ -103,12 +103,12 @@ emptyEnv =
 emitLn :: String -> M ()
 emitLn line = do
     env <- get
-    put $ env { code = line ++ "\n" ++ (code env)) }
+    put $ env { code = line ++ "\n" ++ (code env) }
 
 emit :: String -> M ()
 emit line = do
     env <- get
-    put $ env { code = line ++ (code env)) }
+    put $ env { code = line ++ (code env) }
 
 push :: M ()
 push = do
@@ -124,11 +124,11 @@ pop = do
     return ()
 
 
-generateCode :: Program -> IO()
+generateCode :: Program ->  M ()
 generateCode (PDefs defs) = do
     evalStateT f emptyEnv -- Monad function
     env <- get
-    writeFile test (code env)
+    lift $ writeFile "test" (code env)
     return ()
     where 
         f = do
